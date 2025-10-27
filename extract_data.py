29acfe42f10541c75d60c909b530690b8afe1490
priti <pritikoli1994k@gmail.com>
2025-10-27 20:30:11 +0530

    jen filename added

---
+++ extract_data.py
@@ -0,0 +1,13 @@
+import requests
+import pandas as pd
+
+url = "https://jsonplaceholder.typicode.com/users"  # Example API
+response = requests.get(url)
+
+if response.status_code == 200:
+    data = response.json()
+    df = pd.DataFrame(data)
+    df = df[["id", "name"]]
+    print(df)
+else:
+    print("Failed to fetch data. Status code:", response.status_code)
